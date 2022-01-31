039a6eeab64761d6e40934b8e27148e19c418e73
PhilipWilliams0 <williamsphilip@outlook.com>
2022-01-31 00:28:35 +0000

    Refactored checkout app

--- habitute/settings.py
+++ habitute/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# os.getenv('DEVELOPMENT', '')
+DEBUG = os.getenv('DEVELOPMENT', '')
 
 
 ALLOWED_HOSTS = ['habitute-project.herokuapp.com', 'localhost']

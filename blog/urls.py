5f1bd1a527afaf63ac8bc5d96ba65ddd62185900
PhilipWilliams0 <williamsphilip@outlook.com>
2022-03-07 16:36:58 +0000

    Previously used class base moved for def

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-# from . import views
-from .views import BlogView, BlogDetailView
+from . import views
 
 urlpatterns = [
-    # path("", views.blog, name="blog"),
-    path('', BlogView.as_view(), name="blog"),
-    path('blog/<int:pk>/', BlogDetailView.as_view(), name="blog_detail"),
+    path('', views.blog, name="blog"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Blog
 
 # Create your views here.
-class BlogView(ListView):
-	model = Blog
-	template_name = 'blog/blog.html'
+def blog(request):
+	return render(request, 'blog/blog.html')
 
-
-class BlogDetailView(DetailView):
-	model = Blog
-	template_name = 'blog/blog_details.html'

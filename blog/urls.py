98b0045c7d442d5b31b949250c03b72482e0fb1d
PhilipWilliams0 <williamsphilip@outlook.com>
2022-03-07 16:14:40 +0000

    Create blog templates, views and urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+# from . import views
+from .views import BlogView, BlogDetailView
 
 urlpatterns = [
-    path("", views.blog_home, name="blog"),
+    # path("", views.blog, name="blog"),
+    path('', BlogView.as_view(), name="blog"),
+    path('blog/<int:pk>/', BlogDetailView.as_view(), name="blog_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Blog
 
 # Create your views here.
-def blog_home(request):
-    return render(request, 'blog/blog.html', {})
+class BlogView(ListView):
+	model = Blog
+	template_name = 'blog/blog.html'
+
+
+class BlogDetailView(DetailView):
+	model = Blog
+	template_name = 'blog/blog_details.html'

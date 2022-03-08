d663b5bbac48f844677690f9131a80f047498ea1
PhilipWilliams0 <williamsphilip@outlook.com>
2022-03-08 10:35:34 +0000

    Concluded designing news page

--- newsapp/views.py
+++ newsapp/views.py
@@ -4,17 +4,19 @@ import requests
 
 # Create your views here.
 def news_idx(request):
-    r = requests.get('http://api.mediastack.com/v1/news?access_key=6e62d2407ee16a925395dba5c53def18&keywords=fashion&countries=us,gb')
+    r = requests.get('http://api.mediastack.com/v1/news?access_key=6e62d2407ee16a925395dba5c53def18&keywords=fashion&countries=gb')
     res = r.json()
     data = res['data']
     title = []
     description = []
     image = []
     url = []
+    published_at = []
     for i in data:
         title.append(i['title'])
         description.append(i['description'])
         image.append(i['image'])
         url.append(i['url'])
-    news = zip(title, description, image, url)
+        published_at.append(i['published_at'])
+    news = zip(title, description, image, url, published_at)
     return render(request, 'newsapp/news_idx.html', {'news': news})
